2155f006d7395889453e87f7b344384d1849f5b7
mharisgh <hellomharis@gmail.com>
2026-03-09 11:55:51 +0530

    vision

--- components/home/CloudTextSection.jsx
+++ components/home/CloudTextSection.jsx
@@ -5,7 +5,7 @@ const CloudTextSection = () => {
 
         <div id='cloud-text' className="h-[200vh] w-full bg-[url('/homepage/bigtext-clouds-bg.webp')] saturate-0 bg-cover bg-center">
             <div className="max-w-screen-2xl px-8 md:px-22 mx-auto flex items-center justify-center h-full">
-                <p className="font-accent font-semibold text-display-xl ">Apex Model School Sharjah empowers young minds to learn, explore, and grow.
+                <p className="font-accent font-semibold text-display-xl ">Apex Vision Model School Sharjah empowers young minds to learn, explore, and grow.
 A premium new venture backed by 50+ years of expertise.</p>
             </div>
         </div>
